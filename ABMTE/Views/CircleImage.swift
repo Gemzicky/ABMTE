f6c900e905089c39c9bc921d3c2d7e6178d2107d
Maroš Gemzický <gemzicky@gmail.com>
2021-01-02 21:59:31 +0100

    second

--- ABMTE/Views/CircleImage.swift
+++ ABMTE/Views/CircleImage.swift
@@ -8,18 +8,13 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
     var body: some View {
-        Image("conservation_hero_large_2x")
+        image
             .resizable()
             .scaledToFit()
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.white, lineWidth: 4))
             .shadow(radius: 7)
-    }
-}
-
-struct CircleImage_Previews: PreviewProvider {
-    static var previews: some View {
-        CircleImage()
     }
 }
