02ec225549a4e5a2622201d15edf541ede7aaefc
Maroš Gemzický <gemzicky@gmail.com>
2021-01-03 00:18:53 +0100

    sixth

--- ABMTE/Model/Landmark.swift
+++ ABMTE/Model/Landmark.swift
@@ -17,8 +17,9 @@ struct Landmark: Hashable, Codable, Identifiable {
     var description: String
     var isFavorite: Bool
     var isFeatured: Bool
+
     var category: Category
-    enum Category: String, CaseIterable, Codable {
+    enum Category: String, CaseIterable, Codable, Hashable {
         case lakes = "Lakes"
         case rivers = "Rivers"
         case mountains = "Mountains"
@@ -29,16 +30,22 @@ struct Landmark: Hashable, Codable, Identifiable {
         case stuff = "stuff"
         case water = "water"
     }
+
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
+    var featureImage: Image? {
+        isFeatured ? Image(imageName + "_feature") : nil
+    }
+
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
             longitude: coordinates.longitude)
     }
+
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
