0a02e496767aa70446308c077583f71352435b73
Maroš Gemzický <gemzicky@gmail.com>
2021-01-02 15:37:07 +0100

    Initial Commit

---
+++ ABMTE/ABMTEApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABMTEApp.swift
+//  ABMTE
+//
+//  Created by Maroš Gemzický on 02/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ABMTEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
