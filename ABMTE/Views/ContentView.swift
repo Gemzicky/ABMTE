7e0b7873bc4a79f4823c08765076aa9ecdb55524
Maroš Gemzický <gemzicky@gmail.com>
2021-01-02 22:02:19 +0100

    second missing one file

---
+++ ABMTE/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ABMTE
+//
+//  Created by Maroš Gemzický on 02/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
