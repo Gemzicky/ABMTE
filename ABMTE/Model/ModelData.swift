ea32de3d8a1ab8cb1b2badb3d597b0aecb911738
Maroš Gemzický <gemzicky@gmail.com>
2021-01-04 17:16:46 +0100

    permanent storage userdefaults, at last..

--- ABMTE/Model/ModelData.swift
+++ ABMTE/Model/ModelData.swift
@@ -11,7 +11,15 @@ import Combine
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
-    @Published var profile = Profile.default
+//    @Published var profile = Profile.default
+    @Published var profile = Profile.default {
+        didSet {
+            UserDefaults.standard.set(profile.username, forKey: "username")
+            UserDefaults.standard.set(profile.prefersNotifications, forKey: "prefersNotifications")
+            UserDefaults.standard.set(profile.seasonalPhoto.id, forKey: "seasonalPhotoId")
+            UserDefaults.standard.set(profile.goalDate, forKey: "goalDate")
+        }
+    }
 
     var features: [Landmark] {
         landmarks.filter { $0.isFeatured }
--- ABMTE/Model/Profile.swift
+++ ABMTE/Model/Profile.swift
@@ -6,14 +6,14 @@
 //
 
 import Foundation
-import Combine
 
 class Profile: ObservableObject {
     var username: String
     var prefersNotifications: Bool
     var seasonalPhoto: Season
     var goalDate: Date
-//    static var `default` = Profile(username: "", prefersNotifications: true, seasonalPhoto: Season.spring, goalDate: Date())
+
+    static let `default` = Profile(username: "Anonymous", prefersNotifications: true, seasonalPhoto: Season.spring, goalDate: Date())
 
     enum Season: String, CaseIterable, Identifiable {
         case spring = "🌷"
@@ -25,25 +25,14 @@ class Profile: ObservableObject {
     }
 
     init(username: String, prefersNotifications: Bool, seasonalPhoto: Season, goalDate: Date) {
-//        self.username = ""
-//        self.prefersNotifications = true
-//        self.seasonalPhoto = Season.spring
-//        self.goalDate = Date()
-        self.username = UserDefaults.standard.object(forKey: "username") as? String ?? ""
-        self.prefersNotifications = UserDefaults.standard.object(forKey: "prefersNotifications") as? Bool ?? true
-        self.seasonalPhoto = UserDefaults.standard.object(forKey: "seasonalPhoto") as? Profile.Season ?? Profile.Season.spring
-        self.goalDate = UserDefaults.standard.object(forKey: "goalDate") as? Date ?? Date()
-        self.`default` = Profile (username: "", prefersNotifications: true, seasonalPhoto: Season.spring, goalDate: Date())
-    }
-
-    static var `default` = Profile(username: "", prefersNotifications: true, seasonalPhoto: Season.spring, goalDate: Date())
-    @Published var `default`: Profile {
-        didSet {
-            UserDefaults.standard.set(`default`.username, forKey: "username")
-            UserDefaults.standard.set(`default`.prefersNotifications, forKey: "prefersNotifications")
-            UserDefaults.standard.set(`default`.seasonalPhoto, forKey: "seasonalPhoto")
-            UserDefaults.standard.set(`default`.goalDate, forKey: "goalDate")
+        self.username = UserDefaults.standard.string(forKey: "username") ?? "Anonymous"
+        self.prefersNotifications = UserDefaults.standard.bool(forKey: "prefersNotifications")
+        if UserDefaults.standard.string(forKey: "seasonalPhotoId") != nil {
+            self.seasonalPhoto = Profile.Season(rawValue: UserDefaults.standard.string(forKey: "seasonalPhotoId")!)!
+        } else {
+            self.seasonalPhoto = Season.spring
         }
+        self.goalDate = UserDefaults.standard.object(forKey: "goalDate") as? Date ?? Date()
     }
 }
 
